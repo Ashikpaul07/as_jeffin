1ebe7dda2cea66db47e3328f9c23c6fee82e499c
jeffinjohn7344 <jeffinjohn24@karunya.edu.in>
2025-02-25 20:48:31 +0530

    adding comments

--- fact.py
+++ fact.py
@@ -1,10 +1,14 @@
+#defining variables
 num = 7
 factorial = 1
+#using if condition
 if num < 0:
    print("Sorry, factorial does not exist for negative numbers")
 elif num == 0:
    print("The factorial of 0 is 1")
 else:
+#using for loop
    for i in range(1,num + 1):
        factorial = factorial*i
+#printing the result
    print("The factorial of",num,"is",factorial)
